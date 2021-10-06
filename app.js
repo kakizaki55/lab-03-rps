d779afb51f3f4acfb4dc969c21fc056ad2f43f02
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-06 16:27:54 -0700

    sound wokring properly

--- app.js
+++ app.js
@@ -9,26 +9,35 @@ const charmandor = document.getElementById('charmandor');
 const squirtle = document.getElementById('squirtle')
 const bulbasaur = document.getElementById('bulbasaur')
 
+const redSound = document.getElementById('red-sound');
+const blueSound = document.getElementById('blue-sound');
+const greenSound = document.getElementById('green-sound');
+
 
 battleButton.addEventListener('click', () => {
 // console.log('clickyclask')
 const selectedPokemon = document.querySelector('input[type=radio]:checked')
 
-gameText.textContent = checkWhoWon(selectedPokemon.value, randomPokemon().value)
+ gameText.textContent = checkWhoWon(selectedPokemon.value, randomPokemon().value)
 
         if(selectedPokemon.value === 'red'){
             charmandor.classList.remove('hidden')
             squirtle.classList.add('hidden')
             bulbasaur.classList.add('hidden')
+            redSound.play();
+            
         }else if(selectedPokemon.value === 'blue'){
             squirtle.classList.remove('hidden')
             bulbasaur.classList.add('hidden')
             charmandor.classList.add('hidden')
+            blueSound.play();
         }else if(selectedPokemon.value === 'green'){
             bulbasaur.classList.remove('hidden')
             squirtle.classList.add('hidden')
             charmandor.classList.add('hidden')
+            greenSound.play();
         };
+
 // console.log(selectedPokemon.value);
 // console.log(randomPokemon().value)
 })
