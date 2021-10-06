3b05f95a71394d01c3c554d8d5cafb02a6dc0ba8
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-06 16:13:07 -0700

    have pokemon popping up correctly and text of fight

--- app.js
+++ app.js
@@ -3,29 +3,32 @@ import { randomPokemon, checkWhoWon } from './functions.js'
 
 const battleButton= document.getElementById('battle-button');
 
+const gameText = document.getElementById('game-text');
 
-// function checkWhoWon(player, computer){
-//   if(player.value === computer.value){
-//     console.log('it a tie')
-//   }
-  
-// }
-  
-  
+const charmandor = document.getElementById('charmandor');
+const squirtle = document.getElementById('squirtle')
+const bulbasaur = document.getElementById('bulbasaur')
 
-
-// console.log(randomPokemon())
 
 battleButton.addEventListener('click', () => {
 // console.log('clickyclask')
 const selectedPokemon = document.querySelector('input[type=radio]:checked')
 
+gameText.textContent = checkWhoWon(selectedPokemon.value, randomPokemon().value)
+
+        if(selectedPokemon.value === 'red'){
+            charmandor.classList.remove('hidden')
+            squirtle.classList.add('hidden')
+            bulbasaur.classList.add('hidden')
+        }else if(selectedPokemon.value === 'blue'){
+            squirtle.classList.remove('hidden')
+            bulbasaur.classList.add('hidden')
+            charmandor.classList.add('hidden')
+        }else if(selectedPokemon.value === 'green'){
+            bulbasaur.classList.remove('hidden')
+            squirtle.classList.add('hidden')
+            charmandor.classList.add('hidden')
+        };
 // console.log(selectedPokemon.value);
 // console.log(randomPokemon().value)
-
-checkWhoWon(selectedPokemon.value, randomPokemon().value);
-
-
-
-
 })
