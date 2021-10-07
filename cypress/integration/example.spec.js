3d57bdcfc9f70dde2b98f6433da17b92d0170085
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-07 09:09:04 -0700

    last commit

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -5,8 +5,8 @@ describe('Example Test', ()=>{
     });
 
     describe('HTML elements', ()=>{   
-        it('contains the alchemy logo', ()=>{
-            cy.get('.main-logo').should('be.visible');
+        it('easy test', ()=>{
+            cy.get('body').should('be.visible');
         });
     });
 });
