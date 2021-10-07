c81b9a12b5692cc51f0da76cf1bae82735943b9b
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-07 09:27:41 -0700

    eslint fixes

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-import { randomPokemon, checkWhoWon } from './functions.js' 
+import { randomPokemon, checkWhoWon } from './functions.js'; 
 
 
-const battleButton= document.getElementById('battle-button');
+const battleButton = document.getElementById('battle-button');
 const gameText = document.getElementById('game-text');
 
 const charmandor = document.getElementById('charmandor');
-const squirtle = document.getElementById('squirtle')
-const bulbasaur = document.getElementById('bulbasaur')
+const squirtle = document.getElementById('squirtle');
+const bulbasaur = document.getElementById('bulbasaur');
 
 
-const charizard = document.getElementById('charizard')
-const blastoise = document.getElementById('blastoise') 
+const charizard = document.getElementById('charizard');
+const blastoise = document.getElementById('blastoise'); 
 const venusaur = document.getElementById('venusaur');
 
 
@@ -20,67 +20,67 @@ const greenSound = document.getElementById('green-sound');
 
 const winningCount = document.getElementById('winning-count');
 const loseCount = document.getElementById('losing-count');
-let wincount =0;
-let losscount=0;
+let wincount = 0;
+let losscount = 0;
 
-const resetButton =document.getElementById('reset-button')
+const resetButton = document.getElementById('reset-button');
 
 
 battleButton.addEventListener('click', () => {
-// console.log('clickyclask')
-const selectedPokemon = document.querySelector('input[type=radio]:checked')
-const npcPokemon = randomPokemon()
 
- gameText.textContent = checkWhoWon(selectedPokemon.value, npcPokemon.value)
+    const selectedPokemon = document.querySelector('input[type=radio]:checked');
+    const npcPokemon = randomPokemon();
 
-        if(selectedPokemon.value === 'red'){
-            charmandor.classList.remove('hidden')
-            squirtle.classList.add('hidden')
-            bulbasaur.classList.add('hidden')
-            redSound.play();
+    gameText.textContent = checkWhoWon(selectedPokemon.value, npcPokemon.value);
+
+    if (selectedPokemon.value === 'red'){
+        charmandor.classList.remove('hidden');
+        squirtle.classList.add('hidden');
+        bulbasaur.classList.add('hidden');
+        redSound.play();
             
-        }else if(selectedPokemon.value === 'blue'){
-            squirtle.classList.remove('hidden')
-            bulbasaur.classList.add('hidden')
-            charmandor.classList.add('hidden')
-            blueSound.play();
+    } else if (selectedPokemon.value === 'blue'){
+        squirtle.classList.remove('hidden');
+        bulbasaur.classList.add('hidden');
+        charmandor.classList.add('hidden');
+        blueSound.play();
 
-        }else if(selectedPokemon.value === 'green'){
-            bulbasaur.classList.remove('hidden')
-            squirtle.classList.add('hidden')
-            charmandor.classList.add('hidden')
-            greenSound.play();
-        };
+    } else if (selectedPokemon.value === 'green'){
+        bulbasaur.classList.remove('hidden');
+        squirtle.classList.add('hidden');
+        charmandor.classList.add('hidden');
+        greenSound.play();
+    }
         
-        if(gameText.textContent === 'you win'){
-            wincount ++;
-            winningCount.textContent =wincount;
+    if (gameText.textContent === 'you win'){
+        wincount ++;
+        winningCount.textContent = wincount;
 
 
-        }else if(gameText.textContent ==='you lose'){
-            losscount ++;
-            loseCount.textContent = losscount;
-        }
+    } else if (gameText.textContent === 'you lose'){
+        losscount ++;
+        loseCount.textContent = losscount;
+    }
 
 
-        if(npcPokemon.value === 'red'){
-            charizard.classList.remove('hidden')
-            blastoise.classList.add('hidden')
-            venusaur.classList.add('hidden')
+    if (npcPokemon.value === 'red'){
+        charizard.classList.remove('hidden');
+        blastoise.classList.add('hidden');
+        venusaur.classList.add('hidden');
             
             
-        }else if(npcPokemon.value === 'blue'){
-            blastoise.classList.remove('hidden')
-            venusaur.classList.add('hidden')
-            charizard.classList.add('hidden')
+    } else if (npcPokemon.value === 'blue'){
+        blastoise.classList.remove('hidden');
+        venusaur.classList.add('hidden');
+        charizard.classList.add('hidden');
             
 
-        }else if(npcPokemon.value === 'green'){
-            venusaur.classList.remove('hidden')
-            blastoise.classList.add('hidden')
-            charizard.classList.add('hidden')
+    } else if (npcPokemon.value === 'green'){
+        venusaur.classList.remove('hidden');
+        blastoise.classList.add('hidden');
+        charizard.classList.add('hidden');
             
-        }
+    }
         
 
 
@@ -89,20 +89,20 @@ const npcPokemon = randomPokemon()
 // console.log(randomPokemon().value)
 
 
-})
+});
 resetButton.addEventListener('click', ()=>{
-    wincount =0;
-    losscount=0;
+    wincount = 0;
+    losscount = 0;
     
     loseCount.textContent = losscount;
-    winningCount.textContent =wincount;
+    winningCount.textContent = wincount;
 
-    blastoise.classList.add('hidden')
-    charizard.classList.add('hidden')
-    venusaur.classList.add('hidden')
+    blastoise.classList.add('hidden');
+    charizard.classList.add('hidden');
+    venusaur.classList.add('hidden');
 
-    squirtle.classList.add('hidden')
-    charmandor.classList.add('hidden')
-    bulbasaur.classList.add('hidden')
+    squirtle.classList.add('hidden');
+    charmandor.classList.add('hidden');
+    bulbasaur.classList.add('hidden');
 
-})
+});
--- functions.js
+++ functions.js
@@ -1,46 +1,42 @@
-// const computerPokemon =[
-//     {value:'charzard'},
-//     {value:'blastoise'},
-//     {value:'blastoise'}
-//   ]
+
 
 export function randomPokemon() {
-    const computerPokemon =[
-    {name:'charzard', value:'red'},
-    {name:'blastoise', value:'blue'},
-    {name:'ivysaur', value:'green'}
-    ]
+    const computerPokemon = [
+        { name:'charzard', value:'red' },
+        { name:'blastoise', value:'blue' },
+        { name:'ivysaur', value:'green' }
+    ];
 
-    return computerPokemon[Math.floor(Math.random()*3)]
+    return computerPokemon[Math.floor(Math.random() * 3)];
     
 }
 
 export function checkWhoWon(player, computer){
-    console.log(player,  computer)
-    if(player === computer){
-      console.log('its a tie')
-      return 'its a tie'
-    }else if (player ==='red' && computer === 'blue'){
-        console.log('you lose')
-        return 'you lose'
-    }else if (player === 'red' && computer === 'green'){
-        console.log('you win')
-        return 'you win'
-    }else if (player ==='blue' && computer === 'green'){
-        console.log('you lose')
-        return 'you lose'
-    }else if (player === 'blue' && computer === 'red'){
-        console.log('you win')
-        return 'you win'
+    // console.log(player, computer);
+    if (player === computer){
+        console.log('its a tie');
+        return 'its a tie';
+    } else if (player === 'red' && computer === 'blue'){
+        console.log('you lose');
+        return 'you lose';
+    } else if (player === 'red' && computer === 'green'){
+        console.log('you win');
+        return 'you win';
+    } else if (player === 'blue' && computer === 'green'){
+        console.log('you lose');
+        return 'you lose';
+    } else if (player === 'blue' && computer === 'red'){
+        console.log('you win');
+        return 'you win';
     }
-    else if (player ==='green' && computer === 'red'){
-        console.log('you lose')
-        return 'you loose'
-    }else if (player === 'green' && computer === 'blue'){
-        console.log('you win')
-        return 'you win'
+    else if (player === 'green' && computer === 'red'){
+        console.log('you lose');
+        return 'you loose';
+    } else if (player === 'green' && computer === 'blue'){
+        console.log('you win');
+        return 'you win';
     }
     
-  }
+}
     
   
