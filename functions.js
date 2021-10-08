fc76971431b82550c788fad3dcfb8ea70d6de218
minoka kakizaki <minoka.kakizaki@gmail.com>
2021-10-07 17:23:54 -0700

    cleaned up a bit of code and hopefully test are cleared

--- functions.js
+++ functions.js
@@ -16,25 +16,17 @@ export function checkWhoWon(player, computer){
     if (player === computer){
         // console.log('its a tie');
         return 'its a tie';
-    } else if (player === 'red' && computer === 'blue'){
-        // console.log('you lose');
-        return 'you lose';
     } else if (player === 'red' && computer === 'green'){
         // console.log('you win');
         return 'you win';
-    } else if (player === 'blue' && computer === 'green'){
-        // console.log('you lose');
-        return 'you lose';
     } else if (player === 'blue' && computer === 'red'){
         // console.log('you win');
         return 'you win';
-    }
-    else if (player === 'green' && computer === 'red'){
-        // console.log('you lose');
-        return 'you loose';
     } else if (player === 'green' && computer === 'blue'){
         // console.log('you win');
         return 'you win';
+    } else {
+        return 'you lose';
     }
     
 }
